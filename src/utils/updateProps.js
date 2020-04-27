6bb9e9460aca691985007cfea7d755bde4cc44f3
josdejong <wjosdejong@gmail.com>
2020-04-27 11:26:38 +0200

    Cleanup console.log

--- src/utils/updateProps.js
+++ src/utils/updateProps.js
@@ -15,11 +15,8 @@ export function createUpdateProps () {
       // process added props
       const prevKeys = new Set(props.map(item => item.key)) // TODO: this is inefficient, creating a set. cache this set?
 
-      console.log('updateProps', { value, prevProps, props})
-
       Object.keys(value).forEach(key => {
         if (!prevKeys.has(key)) {
-          console.log('add key', key)
           props.push({
             id: uniqueId(),
             key
